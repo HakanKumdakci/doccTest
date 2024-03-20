80987cde9dc6b7e81aa69c93c2e798f9920b0fb2
Hakan <hakankumdakci@gmail.com>
2024-03-20 17:21:58 +0300

    testSonn

--- build-docc.sh
+++ build-docc.sh
@@ -10,4 +10,4 @@ xcrun docc process-archive transform-for-static-hosting \
     --output-path ".docs" \
     --hosting-base-path "doccTest" # add with your repo name later
     
-echo '<script>window.location.href += "/documentation/doccTest"</script>' > .docs/index.html
+echo '<script>window.location.href += "/documentation/docctest"</script>' > .docs/index.html
