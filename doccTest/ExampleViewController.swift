7fc789d052e42451116b964548130297efafbde9
Hakan <hakankumdakci@gmail.com>
2024-03-26 15:05:25 +0300

    added doc inside

--- doccTest/ExampleViewController.swift
+++ doccTest/ExampleViewController.swift
@@ -40,6 +40,25 @@ class ExampleViewController: UIViewController {
         setupActionButton()
     }
     
+    /// Eat the provided specialty sloth food.
+    ///
+    /// Sloths love to eat while they move very slowly through their rainforest
+    /// habitats. They're especially happy to consume leaves and twigs, which they
+    /// digest over long periods of time, mostly while they sleep.
+    ///
+    /// When they eat food, a sloth's `energyLevel` increases by the food's `energy`.
+    ///
+    /// - Parameters:
+    ///   - food: The food for the sloth to eat.
+    ///   - quantity: The quantity of the food for the sloth to eat.
+    ///
+    /// - Returns: The sloth's energy level after eating.
+    ///
+    /// - Throws: `SlothError.tooMuchFood` if the quantity is more than 100.
+    func testFUnction() {
+        
+    }
+    
     // MARK: - Setup Methods
     
     /**
